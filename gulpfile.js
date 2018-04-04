bc86e2b4aa1186084b0e9920b52648af7e5d605f
Mostafa Salama <mostafalearning2016@gmail.com>
2018-04-05 00:09:10 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -26,16 +26,6 @@ gulp.task('gen-sw', () => {
     };
     return swPrecache.write(`${path.join(__dirname,'sw.js')}`, swOptions);
 })
-gulp.task('generate-service-worker', function(callback) {
-    var path = require('path');
-    var swPrecache = require('sw-precache');
-    var rootDir = 'app';
-
-    swPrecache.write(path.join(rootDir, 'service-worker.js'), {
-        staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif}'],
-        stripPrefix: rootDir
-    }, callback);
-});
 gulp.task('serve', ['html', 'css', 'js'], () => {
     // initial server with port 8000
     bs.init({
