7f16225a337704da9a75a0c73809f7e35f6e6b25
Mostafa Salama <mostafalearning2016@gmail.com>
2018-04-07 13:49:30 +0200

    fetching specific restaurant with fetch api

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -40,20 +40,12 @@ class DBHelper {
      */
     static fetchRestaurantById(id, callback) {
         // fetch all restaurants with proper error handling.
-        DBHelper.fetchRestaurants((error, restaurants) => {
-            if (error) {
-                callback(error, null);
-            } else {
-                const restaurant = restaurants.find(r => r.id == id);
-                if (restaurant) {
-                    // Got the restaurant
-                    callback(null, restaurant);
-                } else {
-                    // Restaurant does not exist in the database
-                    callback('Restaurant does not exist', null);
-                }
-            }
-        });
+        fetch(`${DBHelper.DATABASE_URL}/${id}`).then(res=>{
+            if(res.ok) return res.json() ;
+            return Promise.reject(`error no restaurant with ${id} id `) ;
+        }).then(restaurant=>callback(null,restaurant))
+            .catch(error=>callback(error,restaurant));
+
     }
 
     /**
