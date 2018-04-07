9a948e89739708b83645864a00437599221e64e6
Mostafa Salama <mostafalearning2016@gmail.com>
2018-04-07 13:32:45 +0200

    fetching restaurants with fetch API instead of XHR

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -16,23 +16,23 @@ class DBHelper {
      * Fetch all restaurants.
      */
     static fetchRestaurants(callback) {
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', DBHelper.DATABASE_URL);
-        xhr.onload = () => {
-            if (xhr.status === 200) {
-                // Got a success response from server!
-                const json = JSON.parse(xhr.responseText);
-                const restaurants = json.restaurants;
+        //fetching the data with fetch API
+        fetch(DBHelper.DATABASE_URL)
+            .then(response => {
+                // the request success
+                if (response.ok) {
+                    return response.json();
+                }
+                return Promise.reject(
+                    `Request failed. Returned status of ${response.status}`
+                );
+            })
+            .then(restaurants => {
                 callback(null, restaurants);
-            } else {
-                // Oops!. Got an error from server.
-                const error = `Request failed. Returned status of ${
-                    xhr.status
-                }`;
+            })
+            .catch(error => {
                 callback(error, null);
-            }
-        };
-        xhr.send();
+            });
     }
 
     /**
@@ -176,7 +176,7 @@ class DBHelper {
      * Restaurant image URL.
      */
     static imageUrlForRestaurant(restaurant) {
-        return `/img/${restaurant.photograph}`;
+        return `/img/${restaurant.id}.jpg`;
     }
 
     /**
