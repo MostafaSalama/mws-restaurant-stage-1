40b123bc2f4627f432e7c10c5af6a17b2c8afa5c
Mostafa Salama <mostafalearning2016@gmail.com>
2018-04-10 14:41:34 +0200

    generating new service worker

--- gulpfile.js
+++ gulpfile.js
@@ -5,77 +5,83 @@ const swPreCache = require('sw-precache');
 const concat = require('gulp-concat');
 const imagemin = require('gulp-imagemin');
 const cleanCss = require('gulp-clean-css');
-const uglify = require('gulp-minify') ;
+const uglify = require('gulp-minify');
 // files paths
 const paths = {
     css: 'css/styles.css',
     js: path.join(__dirname, 'js/*.js'),
     html: path.resolve(`${__dirname}/*.html`)
 };
-
+gulp.task('build', ['css', 'index-scripts', 'restaurant-scripts'], () => {
+    gulp.watch(paths.js, ['index-scripts', 'restaurant-scripts']);
+    gulp.watch(paths.css, ['css']);
+});
 gulp.task('default', ['serve']);
-gulp.task('serve',['css','index-scripts','restaurant-scripts'], () => {
+gulp.task('serve', ['css', 'index-scripts', 'restaurant-scripts'], () => {
     // initial server with port 8000
     bs.init({
         server: {
             baseDir: './'
         },
-        port: 4000,
+        port: 4000
     });
 
     // reload on every change
-    gulp.watch(paths.css,['css']).on('change', bs.reload);
+    gulp.watch(paths.css, ['css']).on('change', bs.reload);
     gulp.watch(paths.html).on('change', bs.reload);
-    gulp.watch(paths.js,['index-scripts','restaurant-scripts']).on('change', bs.reload);
+    gulp
+        .watch(paths.js, ['index-scripts', 'restaurant-scripts'])
+        .on('change', bs.reload);
 });
 // minify css
 gulp.task('css', () => {
-    return gulp.src(paths.css)
+    return gulp
+        .src(paths.css)
         .pipe(cleanCss())
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist'));
 });
 
 // concat all scripts
 // uglify them in one script
 gulp.task('index-scripts', () => {
-    return gulp.src(['js/*.js','!js/restaurant_info.js'])
+    return gulp
+        .src(['js/*.js', '!js/restaurant_info.js'])
         .pipe(concat('mainIndex.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('restaurant-scripts', () => {
-    return gulp.src(['js/*.js','!js/main.js'])
+    return gulp
+        .src(['js/*.js', '!js/main.js'])
         .pipe(concat('restaurant.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist'));
 });
 // generating service worker
 gulp.task('gen-sw', () => {
     const swOptions = {
         staticFileGlobs: [
             './index.html',
-            './restaurant.html',
-            './img/*.{png,svg,gif,jpg}',
-            './dist/mainIndex-min.js',
-            './dist/restaurant-min.js',
+            './restaurant.html*',
+            './dist/*.js',
+            './dist/*.js',
             './dist/*.css',
             './icons/*',
             './manifest.json'
         ],
         stripPrefix: '.',
-        runtimeCaching :[
+        runtimeCaching: [
             {
                 urlPattern: /^https:\/\/maps\.googleapis\.com/,
-                handler:'networkFirst'
+                handler: 'networkFirst'
             }
         ]
     };
     return swPreCache.write(`${path.join(__dirname, 'sw.js')}`, swOptions);
 });
 // minify images
-gulp.task('mini-images',()=>{
-    return gulp.src('./img/*.jpg')
+gulp.task('mini-images', () => {
+    return gulp
+        .src('./img/*.jpg')
         .pipe(imagemin())
-        .pipe(gulp.dest('img'))
-})
+        .pipe(gulp.dest('img'));
+});
